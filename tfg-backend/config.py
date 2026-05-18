7e315e9c4d86146d428a256c01ee0d2792882153
jaumeesoleer <jaumesoler.ja@icloud.com>
2026-05-18 22:59:27 +0200

    tfg

--- tfg-backend/config.py
+++ tfg-backend/config.py
@@ -1,13 +1,16 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    uri = os.environ.get('DATABASE_URL')
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
     SECRET_KEY = os.environ.get('SECRET_KEY') or '12345678'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = os.environ.get('JWR_SECRET_KEY')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_TOKEN_LOCATION = ['cookies']
-    JWT_COOKIE_SECURE = False ##
+    JWT_COOKIE_SECURE = True
     JWT_COOKIE_CSRF_PROTECT = False
     JWT_ACCESS_COOKIE_PATH = '/'
     JWT_REFRESH_COOKIE_PATH = '/api/auth/refresh'
